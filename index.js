985b6bdd414c645f2cb8e1ea8778845ce8d34ac3
Arpit <omagrahari55@gmail.com>
2024-10-10 18:17:36 +0530

    cors error

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import app from "./app.js";
 import {connectDb} from "./db/db.js"
 import Razorpay from "razorpay"
-export const instance = new Razorpay({
-    key_id: process.env.RazarPay_key_id,
-    key_secret: process.env.RazarPay_key_secret,
-  });
+// export const instance = new Razorpay({
+//     key_id: process.env.RazarPay_key_id,
+//     key_secret: process.env.RazarPay_key_secret,
+//   });
 connectDb()
 
 app.listen(process.env.PORT,()=>{
